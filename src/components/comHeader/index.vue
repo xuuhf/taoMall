e8ad2c282c7491c4e20490e4146d0ef439263640
xhf <>
2019-11-21 10:27:36 +0800

    * 样式修改

--- src/components/comHeader/index.vue
+++ src/components/comHeader/index.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="comHeader">
-        <div class="com-left common" v-if="$slots.left">
+        <div class="com-left common" >
             <slot name="left"></slot>
         </div>
-        <div class="com-center common" v-if="$slots.center">
+        <div class="com-center common" >
             <slot name="center"></slot>
         </div>
         <div class="com-right common" v-if="$slots.right">
@@ -19,24 +19,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.comHeader {
-    display: flex;
-    width: 100%;
-    height: 100%;
-    .common {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-    .com-left {
-        margin-left: 10px;
-    }
-    .com-right{
-        margin-right: 10px;
-    }
-    .com-center{
-        flex: 1;
-        margin: 0 10px;
-    }
-}
+@import 'index.scss'
 </style>

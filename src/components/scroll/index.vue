074103fe6470244610d6ec312e1629c18b74f178
xhf <>
2019-12-05 14:23:41 +0800

    * 修改方法

--- src/components/scroll/index.vue
+++ src/components/scroll/index.vue
@@ -64,7 +64,7 @@ export default {
                     el: '.swiper-scrollbar'
                 },
                 on: {
-                    touchMove: this.scroll,
+                    sliderMove: this.scroll,
                     touchEnd: this.touchEnd
                 }
                 // mousewheel: true
